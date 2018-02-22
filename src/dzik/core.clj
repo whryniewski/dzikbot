e35abca90980f0626d1697fee64e37def0cd56ae
whr <whr@sentione.com>
2018-02-22 12:59:08 +0100

    minor cleanup

--- src/dzik/core.clj
+++ src/dzik/core.clj
@@ -7,33 +7,6 @@
          '[clojure.string :as str]
          '[ring.middleware.params :as params])
 
-(defn handler [request]
-  (let [username (get-in request [:params "user_name"])
-        text (get-in request [:params "text"])
-        [all command params-non-trimmed params] (re-matches #"!(\w+)( (.*))?" text)]
-    (cond
-      (re-find #"chuj|dupa|cycki|gówno|gowno|kurw|jeba" text)
-      (response {:username "Policja" 
-                 :icon_url "https://www.staffordshire.police.uk/media/3509/Helmet-Icon-Clear/image/Police_Helmet_200px_blue.png" 
-                 :text (format "Nie przeklinaj, %s!" username)})
-      
-      (re-find #"fuck|damn|frickin|shit" text)
-      (response {:username "Police" 
-                 :icon_url "https://www.staffordshire.police.uk/media/3509/Helmet-Icon-Clear/image/Police_Helmet_200px_blue.png" 
-                 :text (format "Watch your language, %s!" username)})
-      
-      (re-find #"^!fortune.*" text)
-      (response {:username "Wizard"
-                 :text (dzik.fortune/fortune)})
-      
-      (= command "giphy")
-      (response {:username "Giphy"
-                 :text 
-                 (format "![giphy](%s)"
-                         (if params (dzik.giphy/search (str/trim params)) (dzik.giphy/random)))}))))
-
-;; (handler {:params {"user_name" "dupa" "text" "!giphy"}})
-
 (defn giphyhandler [orig-params]
   (let [params (orig-params :params)] 
     (println params)
